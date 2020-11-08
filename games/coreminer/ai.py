e5e366f565c55f644a82472f0e52be402c1ac631
Jeffrey Phillips <sportsperson1999@yahoo.com>
2020-11-08 00:47:59 -0600

    upgrade func

--- games/coreminer/ai.py
+++ games/coreminer/ai.py
@@ -133,7 +133,6 @@ class AI(BaseAI):
             while action(miner):
                 job, state, _ = self.job_map[id(miner)]
                 action = self.state_map[job][state]
-            # consider_upgrade(miner)
             # action(miner)
 
             print("(x, y) = ", miner.tile.x, miner.tile.y)
@@ -214,15 +213,13 @@ class AI(BaseAI):
         job, state, details = self.job_map[id(miner)]
         desired_lvl = 0
 
-        if job == "Ore_miner":
-            if details["job_row"] < 20:
+        if job == "Ore_miner" or job == "Shaft_miner":
+            if details["job_row"] < 18:
                 desired_lvl = 1
             else:
                 desired_lvl = 2
         elif job == "Mass_miner":
             desired_lvl = 1
-        elif job == "Shaft_miner":
-            desired_lvl = 0
         elif job == "Military":
             desired_lvl = 3
         else:
